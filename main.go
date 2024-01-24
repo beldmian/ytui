f3afcbd00edb1d412b3139fd5bab2ebc4f89d685
Belousov Dmitry <beldmian@gmail.com>
2024-01-25 00:36:54 +0300

    Some fixes and change Invidious node

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"io/ioutil"
+	"io"
 	"os"
 	"os/exec"
 
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	channelURL = "https://notyoutube.org/channel/"
+	channelURL = "https://yewtu.be/channel/"
 	videoURL   = "https://www.youtube.com"
 )
 
@@ -25,7 +25,7 @@ func main() {
 		log.Panicln("Cannot open config file")
 	}
 	defer f.Close()
-	data, err := ioutil.ReadAll(f)
+	data, err := io.ReadAll(f)
 	if err != nil {
 		log.Panicln("Error in config file format")
 	}
@@ -42,8 +42,8 @@ func main() {
 
 		e.ForEach("div.pure-u-1.pure-u-md-1-4", func(i int, e *colly.HTMLElement) {
 			var vid Video
-			vid.URL = e.ChildAttr("p > a", "href")
-			vid.Name = e.ChildText("p > a")
+			vid.URL = e.ChildAttr("a", "href")
+			vid.Name = e.ChildText("a > p")
 			channel.Videos = append(channel.Videos, vid)
 		})
 
